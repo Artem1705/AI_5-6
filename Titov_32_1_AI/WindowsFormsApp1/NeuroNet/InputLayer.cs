06af2476903d17faf2f9d5228c74a2d240a55ee7
Artem1705 <artemka21012005@gmail.com>
2025-10-28 01:27:02 +0300

    Update InputLayer.cs

--- Titov_32_1_AI/WindowsFormsApp1/NeuroNet/InputLayer.cs
+++ Titov_32_1_AI/WindowsFormsApp1/NeuroNet/InputLayer.cs
@@ -54,7 +54,40 @@ namespace WindowsFormsApp1.NeuroNet
         }
         public void Shuffling_Array_Rows(double[,] arr)
         {
-
+            int numRows = arr.GetLength(0); // Получаем количество строк
+            int numCols = arr.GetLength(1); // Получаем количество столбцов (чтобы знать размер строки)
+            
+            Random random = new Random(); // Создаем генератор случайных чисел
+            
+            // Итерация с конца до начала массива
+            for (int i = numRows - 1; i >= 0; i--)
+            {
+                // Выбираем случайный индекс j от 0 до i (включительно)
+                int j = random.Next(i + 1);
+            
+                // Теперь нужно поменять местами строку i и строку j
+                // Для этого придется временно сохранить строку i
+                double[] tempRow = new double[numCols];
+            
+                // Копируем строку i во временный буфер
+                for (int col = 0; col < numCols; col++)
+                {
+                    tempRow[col] = arr[i, col];
+                }
+            
+                // Копируем строку j в строку i
+                for (int col = 0; col < numCols; col++)
+                {
+                    arr[i, col] = arr[j, col];
+                }
+            
+                // Копируем временный буфер (старую строку i) в строку j
+                for (int col = 0; col < numCols; col++)
+                {
+                    arr[j, col] = tempRow[col];
+                }
+            }
         }
     }
 }
+
